c9633e54ee66add1f81ef86e320cf856e672d2aa
kuzGo <gorankuzma467@gmail.com>
2021-03-29 21:35:44 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,50 @@
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
+
+function getData(type , cb) {
+
+var xhr =  new XMLHttpRequest();
+// let data;
+// function setData(jsonData){
+// }
+
+xhr.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200){
+        cb(JSON.parse(this.responseText));
+        
+    }
+};
+xhr.open("GET", baseURL + type + "/");
+xhr.send();
+}
+
+function getTableHeaders (obj) {
+    let tableHeaders = [];
+Object.keys(obj).forEach(function (key) {
+    tableHeaders.push(`<td>${key}</td>`);            
+ });
+
+ return`<tr>${tableHeaders}</tr>`;
+}
+
+
+function writeDataToDocument (type){
+    let tableRows = [];
+    let el = document.getElementById("data");
+    el.innerHTML ="";
+    getData(type,function(data){
+        data = data.results;
+        let tableHeaders = getTableHeaders(data[0]);
+        data.forEach(function(item) {
+            let dataRow = [];
+            Object.keys(item).forEach(function(key){
+                let rowData = item[key].toString();
+                let truncatedData  = rowData.substring(0,15);
+
+                dataRow.push(`<td>${truncatedData}</td>`)
+            });
+            tableRows.push(`<tr>${dataRow}</tr>`);
+        });
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+    });      
+}
+
